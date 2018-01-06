d7d7061e4b5e2b63159d6341e1572dff7e7ee55a
João Paulo Pulga <joaopaulo1339@outlook.com>
2018-01-06 22:25:49 +0000

    Remove multedit code

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- Load the diff plugin
 _M.file_diff = require('file_diff')
 -- Load multiedit
-local multiedit = require('Multiedit')
+require('Multiedit')
 
 -- Set Text Adept Theme
 if not CURSES then
@@ -10,26 +10,6 @@ if not CURSES then
 		fontsize = 14
 	})
 end
-
-
--- mimics sublimes ctrl-d
-keys.cd = multiedit.select_word
-keys.af3 = multiedit.select_all_words
-
--- exchange all standard bindings with the multiedit version
-keys['\n'] = multiedit.newline
-keys.left = multiedit.char_left
-keys.cleft = multiedit.word_left
-keys.sleft = multiedit.char_left_extend
-keys.csleft = multiedit.word_left_extend
-keys.right = multiedit.char_right
-keys.sright = multiedit.char_right_extend
-keys.cright = multiedit.word_right
-keys.csright = multiedit.word_right_extend
-keys.home = multiedit.line_start
-keys.shome = multiedit.line_start_extend
-keys["end"] = multiedit.line_end
-keys["send"] = multiedit.line_end_extend
 
 -- Text adept editor settings
 textadept.editing.auto_indent = true
