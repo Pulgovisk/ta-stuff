890c6d4f0eb3a019e6330be4472691b8138bc0e8
João Paulo Pulga <joaopaulo1339@outlook.com>
2018-08-24 13:50:41 -0300

    Updated for Textadept 10

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ require('Multiedit')
 
 -- Set Text Adept Theme
 if not CURSES then
-	ui.set_theme('base16-solarized-dark', {
+	buffer:set_theme('base16-solarized-dark', {
 		font = "DejaVu Sans Mono",
 		fontsize = 14
 	})
@@ -14,3 +14,14 @@ end
 -- Text adept editor settings
 textadept.editing.auto_indent = true
 textadept.editing.strip_trailing_spaces = true
+
+-- Disable the horizontal and vertical scrollbars
+buffer.h_scroll_bar = false
+buffer.v_scroll_bar = false
+
+-- Use tabs insted spaces for identation
+buffer.use_tabs = true
+buffer.tab_width = 4
+
+-- Set the multipaste option to EACH, but, for now, this dont works as spected
+buffer.multi_paste = buffer.MULTIPASTE_EACH
--- properties.lua
+++
@@ -1,9 +0,0 @@
--- Disable the horizontal and vertical scrollbars
-buffer.h_scroll_bar = false
-buffer.v_scroll_bar = false
--- Use tabs insted spaces for identation
-buffer.use_tabs = true
--- Set tab size
-buffer.tab_width = 4
--- Set the multipaste option to EACH, but, for now, this dont works as spected
-buffer.multi_paste = buffer.MULTIPASTE_EACH
